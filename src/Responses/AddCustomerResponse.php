c396add03640b7c785a2fe0287c0b78dc164f0ed
Alexander Emelyanov <alexander.emelyanov@outlook.com>
2016-07-11 22:30:52 +0200

    ApiClient::addCustomer method updated.

--- src/Responses/AddCustomerResponse.php
+++ src/Responses/AddCustomerResponse.php
@@ -7,11 +7,40 @@ use Solaris\Response;
 
 class AddCustomerResponse extends Response
 {
+    protected $id;
+
+    protected $authUrl;
+
     public function init()
     {
         $data = $this->payload->getData();
+
         if (isset($data['message']) && $data['message'] == 'E-mail already exists') {
             throw new EmailAlreadyExistsException($this, $data['message']);
         }
+
+        $this->id = $data['id'];
+
+        $this->authUrl = $data['authUrl'];
+    }
+
+    /**
+     * Returns ID of created user from trade platform Solaris
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Returns auto-login URL for created user.
+     *
+     * @return string
+     */
+    public function getAuthUrl()
+    {
+        return $this->authUrl;
     }
 }
